5aad92e7bfb6508519c42fed693c140d5c8623ef
Slamchillz <mendyslam@gmail.com>
2022-09-23 05:05:47 +0000

    removed an unprecedented directory

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# A bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-curl -s -I -L "$1" | grep -i "content-length:" | sed -E 's/content-length:(\s)*//I'
