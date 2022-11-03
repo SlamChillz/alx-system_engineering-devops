86db09186f755033bede7aeebb6ebfa2c68663e4
Slamchillz <mendyslam@gmail.com>
2022-11-03 05:18:06 +0000

    wrong file deleted

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,5 +0,0 @@
-# fix wordpress server
-exec { 'fix-wordpress':
-  command => 'cp /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-  path    => '/usr/local/bin/:/bin/'
-}
