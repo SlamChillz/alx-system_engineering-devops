c65d34ea2cb41e691f822747763509517d55362e
Slamchillz <mendyslam@gmail.com>
2022-11-13 13:08:24 +0000

    could be smarter

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,12 @@
 # Fixes an nginx site that can't handle multiple concurrent requests
+
 exec { 'ulimit':
-  command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
-/etc/default/nginx; service nginx restart\"",
-  path    => '/usr/bin:/usr/sbin:/bin'
+  command => "sed -i 's/^ULIMIT=.*/ULIMIT=\"-n 500\"/' /etc/default/nginx",
+  path    => '/usr/bin:/usr/sbin:/bin',
+  notify  => Service['nginx']
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
